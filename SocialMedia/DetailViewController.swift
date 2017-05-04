38b1f1cf74bf010d8619b88ad674d832bb759eaa
Owen Duignan <owenied@me.com>
2017-05-04 18:14:53 -0500

    first commit - reusing initial Storm Viewer app files also

---
+++ SocialMedia/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  SocialMedia
+//
+//  Created by Owen Duignan on 04/05/2017.
+//  Copyright © 2017 Owen Duignan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
